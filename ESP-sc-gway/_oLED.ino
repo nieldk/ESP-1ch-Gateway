024e90c3a48a17dea56304680844a446208e2c3c
Niel Nielsen <nieldk@gmail.com>
2019-05-15 08:37:55 +0200

    Add files via upload

---
+++ ESP-sc-gway/_oLED.ino
@@ -0,0 +1,107 @@
+// 1-channel LoRa Gateway for ESP8266
+// Copyright (c) 2016, 2017, 2018 Maarten Westenberg version for ESP8266
+// Version 5.3.3
+// Date: 2018-08-25
+//
+// 	based on work done by Thomas Telkamp for Raspberry PI 1ch gateway
+//	and many others.
+//
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the MIT License
+// which accompanies this distribution, and is available at
+// https://opensource.org/licenses/mit-license.php
+//
+// NO WARRANTY OF ANY KIND IS PROVIDED
+//
+// Author: Maarten Westenberg (mw12554@hotmail.com)
+//
+// This file contains the state machine code enabling to receive
+// and transmit packages/messages.
+// ========================================================================================
+//
+
+#if OLED>=1
+
+
+// ----------------------------------------------------------------	
+// Initilize the OLED functions.
+//
+// ----------------------------------------------------------------
+void init_oLED() 
+{
+#if defined OLED_RST
+	pinMode(OLED_RST,OUTPUT);
+	digitalWrite(OLED_RST, LOW); 	// low to reset OLED
+	delay(50); 
+	digitalWrite(OLED_RST, HIGH); 	// must be high to turn on OLED
+	delay(50);
+#else
+#endif
+	// Initialising the UI will init the display too.
+	display.init();
+	display.flipScreenVertically();
+	display.setFont(ArialMT_Plain_24);
+	display.setTextAlignment(TEXT_ALIGN_LEFT);
+	display.drawString(0, 14, "STARTING");
+	display.display();
+}
+
+// ----------------------------------------------------------------
+// Activate the OLED
+//
+// ----------------------------------------------------------------
+void acti_oLED() 
+{
+	// Initialising the UI will init the display too.
+	display.clear();
+	
+#if OLED>=1
+	display.setFont(ArialMT_Plain_16);
+	display.drawString(0, 6, "READY");
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(0, 22, "SSID=");
+	display.drawString(30, 22, WiFi.SSID());
+  display.drawString(0, 32, "IP=");
+  display.drawString(14, 32, WiFi.localIP().toString());
+//#elif OLED==2
+//  display.setFont(ArialMT_Plain_16);
+//  display.drawString(0, 0, "READY");
+//  display.setFont(ArialMT_Plain_10);
+//  display.drawString(0, 16, "SSID=");
+//  display.drawString(40, 26, WiFi.SSID());
+//  display.drawString(0, 36, "IP=");
+//  display.drawString(30, 36, WiFi.localIP().toString());
+#endif
+
+	display.display();
+}
+
+// ----------------------------------------------------------------
+// Print a message on the OLED.
+// Note: The whole message must fit in the buffer
+//
+// ----------------------------------------------------------------
+void msg_oLED(String tim, String sf) {
+    display.clear();
+    display.setFont(ArialMT_Plain_16);
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+	
+	display.drawString(0, 48, "LEN: " );
+//    display.drawString(40, 48, String((int)messageLength) );
+    display.display();
+	yield();
+}
+
+// ----------------------------------------------------------------
+// Print the OLED address in use
+//
+// ----------------------------------------------------------------
+void addr_oLED() 
+{
+	Serial.print(F("OLED_ADDR=0x"));
+	Serial.println(OLED_ADDR, HEX);
+}
+
+
+
+#endif
